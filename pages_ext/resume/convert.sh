22b9a838f61ba06a557790dd7a36ba8098d8a12a
Matt Bierbaum <matt.bierbaum@gmail.com>
2019-03-04 20:20:44 -0500

    updating resume

--- pages_ext/resume/convert.sh
+++ pages_ext/resume/convert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-phantomjs ~/bin/rasterize.js http://127.0.0.1:8080/pages_ext/resume/resume.html resume.pdf A4
+phantomjs rasterize.js http://127.0.0.1:8080/pages_ext/resume/resume.html resume.pdf A4
